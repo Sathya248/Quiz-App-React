5c3fa949c44606f184b33d09e7f2e673cdee970b
Sathya248 <sathyakannan248@gmail.com>
2024-09-10 14:44:57 +0530

    Quiz-app

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,40 @@
+export const Questions =[
+    {
+        prompt: "What is 1+3?",
+        optionA: "24",
+        optionB: "21",
+        optionC: "4",
+        optionD: "2",
+        answer: "C",
+    },
+
+    {
+        prompt: "Which is a Streaming Platform?",
+        optionA: "Twitter",
+        optionB: "Twitch",
+        optionC: "Google",
+        optionD: "Robinhood",
+        answer: "B",
+    },
+
+    {
+        prompt: "Best tech Youtube channel?",
+        optionA: "PedroTech",
+        optionB: "Dev Ed",
+        optionC: "Fireship",
+        optionD: "TechWithTim",
+        answer: "optionA",
+    },
+
+    {
+        prompt: "What is 20-13?",
+        optionA: "7",
+        optionB: "21",
+        optionC: "4",
+        optionD: "2",
+        answer: "A",
+    },
+
+]
+
+// export default QuestionBank
